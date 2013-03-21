1f76ca5433e4b01000c17e35e2ebcfb276e1a784
Hai-Nam Nguyen <hainam@jcisio.com>
2013-03-21 10:19:18 +0100

    Use README.md for help

--- bookmarks.sh
+++ bookmarks.sh
@@ -577,55 +577,24 @@ gh ()
 #-------------------------------------------------------------------------------
 bookmarks ()
 {
-  declare usage="
-  DIRECTORY BOOKMARKS FOR BASH (version $BBM_Version)
-
-  b  [bookmark [dir]]      : bookmark a directory
-                               <no option>   bookmark the current directory
-                               bookmark      use bookmark for current directory
-                               bookmark dir  use bookmark for given directory
-  bl [-d][-t][regex]       : show the bookmark list (tab compl.)
-                               -d            dictionary order
-                               -t            show time stamps
-                               regex         list bookmarks matching regex
-  bm bookmark              : resolve a bookmark (tab compl.)
-  g  [bookmark]            : go to a bookmark or named directory (tab compl.)
-                               <no option>   go to the home directory
-                               bookmark      go to the bookmarked directory
-  p  [bookmark]            : push bookmark / directory onto dir stack (tab compl.)
-                               bookmark      pushd bookmarked directory
-  r  [bookmark|regex]      : remove a saved bookmark(s) (tab compl.)
-                               bookmark      remove bookmark
-                               regex         remove bookmarks matching regex
-
-  gh [bookmark]            : go to a Midnight Commander hotlist entry (tab compl.)
-                             use ssh for a SHell filesystem link
-                               <no option>   go to the home directory
-                               bookmark      go to the bookmarked directory
-  hl [-d][regex]           : Midnight Commander directory hotlist (tab compl.)
-                               -d            dictionary order
-                               regex         list bookmarks matching regex
-
-  bookmarks [-h]           : this help message
-  bookmarks  -b bmfile     : use bookmark file bmfile
-  bookmarks  -i            : import Midnight Commander hotlist
-  bookmarks  -r [bash|mc]  : reread Bash/MC bookmark file
-  bookmarks  -v            : display the version of this script
-  bookmarks  -e [bookmark] : export a shell variable from every bookmark
-                           : export a shell variable from given bookmark
-
-  The actual bookmark file is '$BBM_BOOKMARKFILE'.
-  Reguluar expressions are explained in  manual regex(7).
-  "
-
   declare    option
   declare    OPTIND OPTARG                      # do not modify global variables
   declare -r OPTIONSTRING=':hvb:eir'
   declare    bm timestamp
   declare -i count=0
 
+  # Get the script directory to read the README file, also resolve symlinks.
+  SOURCE="${BASH_SOURCE[0]}"
+  while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+    DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+    SOURCE="$(readlink "$SOURCE")"
+    [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE"
+  done
+  DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+
   if [ $# -eq 0 ] ; then
-    printf "%s\n" "$usage"
+    printf "\nDIRECTORY BOOKMARKS FOR BASH (version %s)\n\n" $BBM_Version
+    sed -n '/^BOOKMARK/,/^REGEX/p' $DIR/README.md | sed '1,11d' | sed '$d'
     return 0
   fi
 
